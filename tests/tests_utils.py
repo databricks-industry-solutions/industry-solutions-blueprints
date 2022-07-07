46970ef2bbaaa02fa2254ad7b93e898f66843c90
Antoine Amend <antoine.amend@gmail.com>
2022-07-06 17:46:51 -0700

    template config

--- tests/tests_utils.py
+++ tests/tests_utils.py
@@ -4,10 +4,10 @@ import unittest
 
 class UtilTest(unittest.TestCase):
 
-    def test_hello(self):
-	self.assertFalse('hello' == 'world')
-        print('Hello World')
+  def test_hello(self):
+    self.assertFalse('hello' == 'world')
+    print('Hello World')
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
