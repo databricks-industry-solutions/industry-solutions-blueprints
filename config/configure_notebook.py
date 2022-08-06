07317ecda7c85a6dccac9d19d4599668c33eafba
dbbnicole <86326159+dbbnicole@users.noreply.github.com>
2022-08-05 20:50:19 -0500

    Delete config directory

--- config/configure_notebook.py
+++
@@ -1,9 +0,0 @@
-# Databricks notebook source
-# MAGIC %pip install -r requirements.txt
-
-# COMMAND ----------
-
-import yaml
-with open('config/application.yaml', 'r') as f:
-  config = yaml.safe_load(f)
-
