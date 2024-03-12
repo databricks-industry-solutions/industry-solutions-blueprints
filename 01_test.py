88c6705810686355e124bf0b95e7ec6d8274d7f7
Antoine Amend <antoine.amend@gmail.com>
2024-03-12 09:44:05 -0700

    Adding publish runner

--- 01_test.py
+++ 01_test.py
@@ -1,3 +1,9 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC # Databricks notebook source
+# MAGIC
+# MAGIC Separate business logic into specific markdown sections (ensure space after pound sign)
 
-Separate business logic into specific markdown sections (ensure space after pound sign)
+# COMMAND ----------
+
+print('Hello, Databricks')
